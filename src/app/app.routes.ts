3cde87c05f8b7afa2ed6411dea073c0be2c4c022
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-12 13:56:10 -0300

    Inserting Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Register } from './register/register';
+import { Consult } from './consult/consult';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'register', component: Register },
+    { path: 'consult', component: Consult }
+];
