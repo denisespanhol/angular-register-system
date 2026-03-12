5f1041818903e5c5baa9ce9598cf6a9b9a344350
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-12 11:06:57 -0300

    Creating component consult and register

---
+++ src/app/consult/consult.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consult',
+  imports: [],
+  templateUrl: './consult.html',
+  styleUrl: './consult.scss',
+})
+export class Consult {}
