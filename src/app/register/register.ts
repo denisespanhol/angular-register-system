4c343366be25a9279537ae7c65dffa7c76aed436
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-13 13:39:59 -0300

    Starting build register form

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ 
+    FlexLayoutModule, 
+    MatCardModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    FormsModule 
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
