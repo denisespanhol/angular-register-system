b83809dd8c342b4202da13b2e0c1f4953fd87551
Denis Espanhol <denisgfespanhol@gmail.com>
2026-03-16 11:55:43 -0300

    updating html and css

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -3,6 +3,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -12,6 +14,8 @@ import { FormsModule } from '@angular/forms';
     MatCardModule, 
     MatFormFieldModule, 
     MatInputModule, 
+    MatIconModule,
+    MatButtonModule,
     FormsModule 
   ],
   templateUrl: './register.html',
